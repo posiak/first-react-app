2e78d2c25277486007ea94e0b92edf7a90bf75a0
posiak <barbaraposiak@gmail.com>
2020-10-12 20:14:15 +0200

    Updates

---
+++ precache-manifest.e74d1989c639614557dbafe079c8ef5c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7448c85b06f571a4387169dac57aa4cc",
+    "url": "/posiak/first-react-app/index.html"
+  },
+  {
+    "revision": "6f31de8ec68136e6fbb0",
+    "url": "/posiak/first-react-app/static/css/main.5270dd42.chunk.css"
+  },
+  {
+    "revision": "1a8c72aea1728799ee5b",
+    "url": "/posiak/first-react-app/static/js/2.c3250c3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/posiak/first-react-app/static/js/2.c3250c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f31de8ec68136e6fbb0",
+    "url": "/posiak/first-react-app/static/js/main.7944c6b5.chunk.js"
+  },
+  {
+    "revision": "e5a241ed4c25d675fa79",
+    "url": "/posiak/first-react-app/static/js/runtime-main.d77616a1.js"
+  },
+  {
+    "revision": "4bb628c844f3fb26e83999d4ba788b43",
+    "url": "/posiak/first-react-app/static/media/header1.4bb628c8.jpg"
+  },
+  {
+    "revision": "3192ee6643926b1582040c7070f00e51",
+    "url": "/posiak/first-react-app/static/media/header2.3192ee66.jpg"
+  },
+  {
+    "revision": "4efd186aa608ba7c0f0185c68a6b22d8",
+    "url": "/posiak/first-react-app/static/media/header3.4efd186a.jpg"
+  }
+]);
